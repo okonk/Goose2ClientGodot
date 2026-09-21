7f4bc583f57ba3af27f5c14d5c0bb30708534503
Hayden Curran <haydenz@gmail.com>
2026-09-21 10:15:01 +0000

    feat(minimap): whole-map 1px/tile average-color bitmap builder

---
+++ Scripts/Map/MinimapBitmapBuilder.cs
@@ -0,0 +1,17 @@
+using System;
+using Godot;
+using MapEditor.Core;
+
+namespace Goose2Client.Map;
+
+public static class MinimapBitmapBuilder
+{
+    public static Image Build(MapDocument map, Func<int, int, Color?> provider)
+    {
+        var image = Image.Create(map.Width, map.Height, false, Image.Format.Rgba8);
+        for (int y = 0; y < map.Height; y++)
+            for (int x = 0; x < map.Width; x++)
+                image.SetPixelv(new Vector2I(x, y), MinimapColors.PickColor(map[x, y], provider) ?? Colors.Black);
+        return image;
+    }
+}
